675a5b373b86b193a50e5fc912a1a4ceb4f9eab3
qe-present <2664481691@qq.com>
2025-04-19 23:31:07 +0800

    add

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
+use tauri::{Manager,command};
+#[command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
 fn main() {
-    tw_lib::run()
+
+
+    tauri::Builder::default()
+        .setup(|app|{
+            let window=app.get_webview_window("main").unwrap();
+            window.open_devtools();
+            Ok(())
+
+        })
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
 import "./App.css";
-import init,{get_theme} from "../src-wasm/window/pkg/src_wasm";
+import init,{get_name,get_version} from "../src-wasm/app/pkg/app";
 import {useEffect} from "react";
 function App() {
     useEffect(() => {
         init()
     }, []);
 
-    async function greet() {
-        let a=await get_theme();
-        console.log(a);
+    async function greet(num:Number) {
+        switch (num){
+            case 1:
+                let name = await get_name();
+                console.log(name);
+                break;
+            case 2:
+
+            default:
+                console.log("default");
+                break;
+        }
     }
 
     return (
         <>
             <main className="container">
                 <h1>Welcome to Tauri + React</h1>
-                <button onClick={()=> greet()}>点击</button>
+                <button onClick={()=> greet(1)}>点击</button>
             </main>
         </>
 
